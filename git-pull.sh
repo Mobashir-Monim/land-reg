cc0ebf4fc0ff8edaa850b1fbb0f1f03623261444
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2019-12-15 12:49:27 +0600

    Remove all git tests

--- git-pull.sh
+++
@@ -1,1 +0,0 @@
-sudo -u www-data git pull ; echo 'mobashirmonim@gmail.com' ; echo '$00393294756134@cCx'
--- routes/web.php
+++ routes/web.php
@@ -67,13 +67,6 @@ Route::get('test', function () {
 
 Route::get('/test2', 'MineController@processMine');
 Route::get('/test3', function () {
-    // $before = exec('whoami');
-    // $after = exec('sudo su ; whoami');
-    // $pull = exec('git pull');
-    // dd(exec("sudo -u www-data git pull ; echo 'mobashirmonim@gmail.com' ; echo '$00393294756134@cCx'"));
-    // dd($before, $after, $pull);
-    // dd('Nothing in test');
-    dd(exec("cd .. ; ./git-pull.sh"));
     return view('test3');
 });
 // Route::get('/test2', function () {
