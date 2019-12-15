2619476af71c8cafddbb28a905d29d03bf826151
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2019-12-15 12:14:52 +0600

    Test git pull

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::get('test', function () {
 
 Route::get('/test2', 'MineController@processMine');
 Route::get('/test3', function () {
-    dd(exec('sudo su ; git pull ; pwd'));
+    dd(exec('whoami ; sudo su ; whoami ; git pull'));
     // dd('Nothing in test');
     return view('test3');
 });
