d41b2dba37a636aee37f4e24a19e063846b26b97
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2019-12-18 01:47:13 +0600

    debug

--- app/ServerConfig.php
+++ app/ServerConfig.php
@@ -11,7 +11,7 @@ class ServerConfig extends BaseModel
         try {
             return self::getConf($name)->value;
         } catch (\Exception $e) {
-            return [null, $e];
+            return [null, $e, $name, self::getConf($name)];
         }
     }
 
