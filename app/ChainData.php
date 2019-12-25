688c60498f24bc0a105782ba860e6de4eb26f3ef
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2019-12-26 03:45:31 +0600

    Make model fillable

--- app/ChainData.php
+++ app/ChainData.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ChainData extends Model
+class ChainData extends BaseModel
 {
     protected $primaryKey = null;
     public $incrementing = false;
