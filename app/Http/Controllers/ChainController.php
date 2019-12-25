ee1fe52acfd9795763fe9b1e1279c136626c735b
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2019-12-26 05:10:57 +0600

    fix error

--- app/Http/Controllers/ChainController.php
+++ app/Http/Controllers/ChainController.php
@@ -22,12 +22,12 @@ class ChainController extends Controller
 
             foreach ($councils as $council) {
                 $response = $this->postData("http://$council->ip/api/blocks/chain/$txid/process", ['ip' => $self->ip, 'chain_data' => json_encode($chainData->data)]);
-                $message .= "$council->ip/blocks";
+                $message .= "http://$council->ip/blocks ";
             }
         } else {
             $parent = $self->parent;
             $response = $this->postData("http://$parent->ip/api/blocks/chain/$txid/process", ['ip' => $self->ip, 'chain_data' => $chainData->data]);
-            $message .= "$parent->ip/blocks/chain/$txid/request";
+            $message .= "http://$parent->ip/blocks/chain/$txid/request";
         }
 
         $chainData->requested = true;
