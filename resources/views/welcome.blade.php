7e2f7639a1ef29612bf3fa0fb08b74c6f068b1c6
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2019-12-27 14:00:56 +0600

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -134,5 +134,5 @@
         </div>
     </div>
 
-    <iframe src="http://tradecapacity.pri-bd.org/iframe/useful-links" width="100%" height="600px"></iframe>
+    <iframe src="http://www.tradecapacity.pri-bd.org/iframe/useful-links" style="border:0px #ffffff none;" name="myiFrame" scrolling="yes" frameborder="0" marginheight="0px" marginwidth="0px" height="100%" width="600px" allowfullscreen></iframe>
 @endsection
