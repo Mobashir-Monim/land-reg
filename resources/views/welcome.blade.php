c207b30b298c0efa1e27b4ff11470097dc46b9d9
Mobashir_Monim@bitbucket.org <mobashirmonim@gmail.com>
2019-12-27 20:56:24 +0600

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -134,5 +134,5 @@
         </div>
     </div>
 
-    <iframe src="http://tradecapacity.pri-bd.org/iframe/training-programs" width="100%" height="600px"></iframe>
+    <iframe src="https://www.taff2.com/iframe/training-programs" width="100%" height="600px"></iframe>
 @endsection
